6df72c11fd2aebbb50aa220207ddce02bc849c71
R4z1xx <t.bertrand@groupe-aen.info>
2024-05-20 21:47:32 +0200

    Fix url regex

--- malwatcher-worker/tools/ioc_parser.py
+++ malwatcher-worker/tools/ioc_parser.py
@@ -8,10 +8,10 @@ class IOCParser:
             # {"find": r"(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}", "type": "ipv6"}, # Simple one
             {"find": r"^(([0-9a-fA-F]{1,4}:){1,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,6}:|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|([0-9a-fA-F]{1,4}:)(:[0-9a-fA-F]{1,4}){1,6}|:((:[0-9a-fA-F]{1,4}){1,7}|:))$", "type": "ipv6"}, # Any IPv6 format
             {"find": r"(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}", "type": "domain"},
-            {"find": r"^(?:https?://)?(?:[a-z0-9.-]+\.)?[a-z0-9.-]+(?::\d+)?(?:/[^\s]*)?$", "type": "url"},
             {"find": r"^[a-fA-F0-9]{64}$", "type": "sha256"},
             {"find": r"^[a-fA-F0-9]{40}$", "type": "sha1"},
             {"find": r"^[a-fA-F0-9]{32}$", "type": "md5"},
+            {"find": r"^(?:https?://)?(?:[a-z0-9.-]+\.)?[a-z0-9.-]+(?::\d+)?(?:/[^\s]*)?$", "type": "url"},
         ]
         self.ioc_patterns = [{"find": re.compile(i["find"], re.VERBOSE), "type": i["type"]} for i in self.patterns]
 
